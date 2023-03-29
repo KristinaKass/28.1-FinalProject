2577818fb88aae174e6b53886f29093dff3d56a2
KristinaKass <you@example.com5432zx@mail.ru>
2023-03-29 21:26:55 +0700

    Initial commit

---
+++ settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = "/tests/chromedriver.exe"
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 's*****b@bk.ru'
+    valid_password = 'DYqExp!dQXTZUB2'
+    valid_email_reg = 'Ввести валидный email'
+    invalid_email = 'pochta@gmail.ru'
+    invalid_password = 'P@ssword1'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Анна'
+    last_name = 'Каренина'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'а'
+    latin_generate_string = 'incorrect'
+    chinese_chars = '形聲字我喜欢画大自然'
+    special_chars = '!@#$(&*)_+=/{}%^'
+    numbers = 999
+    empty = ''
+    passw1 = 'P@s$word1'
+    passw2 = 'P@s$word2222'
+    passw3 = 'P@s$word33333333'
